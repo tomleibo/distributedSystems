4f31b223f3926fe9233c05b7578645a692a9c1c5
hagai-lvi <hagai.lvi@gmail.com>
2016-04-09 16:32:33 +0300

    Added tests for ec2 closes #4

--- common/src/main/java/com/bgu/dsp/awsUtils/EC2Utils.java
+++ common/src/main/java/com/bgu/dsp/awsUtils/EC2Utils.java
@@ -3,6 +3,7 @@ package com.bgu.dsp.awsUtils;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.services.ec2.AmazonEC2Client;
 import com.amazonaws.services.ec2.model.*;
+import com.bgu.dsp.awsUtils.exceptions.NoSuchInstanceException;
 import org.apache.log4j.Logger;
 
 import java.util.ArrayList;
@@ -58,6 +59,32 @@ public class EC2Utils {
         }
 
         return instanceCount;
+    }
+
+    public static List<String> getAllWorkersIds(){
+        List<String> res = new LinkedList<>();
+
+        DescribeInstancesResult allWorkers = getAllWorkers();
+        for (Reservation reservation : allWorkers.getReservations()) {
+            for (Instance instance : reservation.getInstances()) {
+                res.add(instance.getInstanceId());
+            }
+        }
+
+        return res;
+    }
+
+    public static InstanceState getInstanceState(String instanceId) throws NoSuchInstanceException {
+        DescribeInstancesRequest describeInstancesRequest = new DescribeInstancesRequest().withInstanceIds(instanceId);
+        DescribeInstancesResult describeInstancesResult = ec2.describeInstances(describeInstancesRequest);
+        List<Reservation> reservations = describeInstancesResult.getReservations();
+        if (reservations.size() < 1){
+            throw new NoSuchInstanceException("There is no instance with id " + instanceId);
+        }
+        else{
+            return reservations.get(0).getInstances().get(0).getState();
+        }
+
     }
 
     private static DescribeInstancesResult getAllWorkers() {
@@ -123,7 +150,7 @@ public class EC2Utils {
     /**
      * Wait for a list of instances
      */
-    private static void waitForRunning(List<String> instancesIds){
+    public static void waitForRunning(List<String> instancesIds){
         logger.debug("Waiting for " + instancesIds.size() + " machines to start");
         long startTime = System.currentTimeMillis();
 
@@ -204,7 +231,20 @@ public class EC2Utils {
         RunInstancesResult runInstancesResult = ec2.runInstances(request);
 
         String instancesIds = runInstancesResult.getReservation().getInstances().get(0).getInstanceId();
+        tagManager(instancesIds);
         return instancesIds;
+    }
+
+    private static void tagManager(String instancesId) {
+        List<String> instances = new LinkedList<>();
+        instances.add(instancesId);
+
+        List<Tag> tags = new LinkedList<>();
+        tags.add(new Tag("Name", "manager"));
+
+        CreateTagsRequest createTagsRequest = new CreateTagsRequest(instances, tags);
+        ec2.createTags(createTagsRequest);
+
     }
 
 
@@ -217,6 +257,10 @@ public class EC2Utils {
                 workesrIDs.add(instance.getInstanceId());
             }
         }
+        terminateByIds(workesrIDs);
+    }
+
+    private static void terminateByIds(List<String> workesrIDs) {
         if (workesrIDs.size() == 0) {
             logger.info("No worker discoverd, skipping shutdown");
             return;
@@ -227,5 +271,20 @@ public class EC2Utils {
 
         TerminateInstancesRequest terminateInstancesRequest = new TerminateInstancesRequest(workesrIDs);
         ec2.terminateInstances(terminateInstancesRequest);
+    }
+
+	/**
+     * Terminates the manager machine
+     * If there is no manager ignores the request silently
+     */
+    public static void terminateManager() {
+        Instance managerInstance = getManagerInstance();
+        if (managerInstance == null){
+            return;
+        }
+        String instanceId =  managerInstance.getInstanceId();
+        List<String> ids = new LinkedList<>();
+        ids.add(instanceId);
+        terminateByIds(ids);
     }
 }
---
+++ common/src/test/java/com/bgu/dsp/tests/Ec2Tests.java
@@ -0,0 +1,69 @@
+package com.bgu.dsp.tests;
+
+import com.amazonaws.services.ec2.model.InstanceState;
+import com.bgu.dsp.awsUtils.EC2Utils;
+import com.bgu.dsp.awsUtils.exceptions.NoSuchInstanceException;
+import org.junit.After;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+/**
+ * Created by hagai_lvi on 09/04/2016.
+ */
+public class Ec2Tests {
+
+	@After
+	public void tearDown(){
+		EC2Utils.terminateAllWorkers();
+		EC2Utils.terminateManager();
+	}
+
+	@Test
+	public void startWorkers() throws NoSuchInstanceException {
+		final int NUM_OF_WORKERS = 2;
+		EC2Utils.startWorkersAndWait(NUM_OF_WORKERS);
+
+		List<String> allWorkersIds = EC2Utils.getAllWorkersIds();
+
+		assertEquals(NUM_OF_WORKERS, allWorkersIds.size());
+		for (String id : allWorkersIds) {
+			InstanceState instanceState = EC2Utils.getInstanceState(id);
+			assertEquals(instanceState.getName(),"running");
+		}
+
+	}
+
+	@Test
+	public void startManager() throws NoSuchInstanceException {
+		String managerID = EC2Utils.startManager();
+
+		String instanceState = EC2Utils.getInstanceState(managerID).getName();
+		assertTrue(instanceState.equals("running") || instanceState.equals("pending"));
+
+		repetitiveAssert(managerID);
+	}
+
+	private void repetitiveAssert(String managerID) throws NoSuchInstanceException {
+		long start = System.currentTimeMillis();
+		long THRESHOLD = 1000 * 60;
+
+		do {
+			try {
+				assertEquals("running", EC2Utils.getInstanceState(managerID).getName());
+				return;
+			}
+			catch (AssertionError e){
+				try {
+					Thread.sleep(5 * 1000);
+				} catch (InterruptedException e2) {
+					e.printStackTrace();
+				}
+			}
+		}while (System.currentTimeMillis() - start < THRESHOLD);
+
+		assertEquals("running", EC2Utils.getInstanceState(managerID).getName());
+	}
+
+}
