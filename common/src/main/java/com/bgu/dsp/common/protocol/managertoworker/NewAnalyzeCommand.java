b3dcbc2b761f2d2ecc2931b2bd00c42552e1f490
Tom Leibovich <tomleibo@gmail.com>
2016-04-12 11:33:08 +0300

    Implementing worker

--- common/src/main/java/com/bgu/dsp/common/protocol/managertoworker/NewAnalyzeCommand.java
+++ common/src/main/java/com/bgu/dsp/common/protocol/managertoworker/NewAnalyzeCommand.java
@@ -1,8 +1,16 @@
 package com.bgu.dsp.common.protocol.managertoworker;
 
+import com.bgu.dsp.common.protocol.managertolocal.Tweet;
+import org.apache.log4j.Logger;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
 import java.util.UUID;
 
 public class NewAnalyzeCommand implements ManagerToWorkerCommand {
+
+    final static Logger log = Logger.getLogger(NewAnalyzeCommand.class);
 
 	public UUID getUuid() {
 		return uuid;
@@ -19,6 +27,8 @@ public class NewAnalyzeCommand implements ManagerToWorkerCommand {
 	private final UUID uuid;
 	private final String sqsQueueName;
 	private final String tweetUrl;
+
+
 
 	@Override
 	public String toString() {
@@ -37,7 +47,32 @@ public class NewAnalyzeCommand implements ManagerToWorkerCommand {
 
 	@Override
 	public void execute() {
-		// TODO implement by the workers
-		System.out.println(toString());
+        String title= getTitleFromUrl();
+        Tweet tweet = processTweetFromTitle(title);
+        uploadTweetToQueue(tweet);
 	}
+
+
+    private String getTitleFromUrl() {
+        Document doc;
+        try {
+            doc = Jsoup.connect(tweetUrl).get();
+            String title = doc.title();
+            return title;
+        }
+        catch(IOException e) {
+            log.error("error fetching url", e);
+            throw new RuntimeException("Failed to fetch URL.");
+        }
+    }
+
+    private Tweet processTweetFromTitle(String title) {
+        System.out.println(title);
+        return null;
+    }
+
+    private void uploadTweetToQueue(Tweet tweet) {
+        //WorkerToManagerSQSProtocol.newCompletedMessage(new WorkerToManagerMessage())
+    }
+
 }
---
+++ worker/src/main/java/Worker.java
@@ -0,0 +1,48 @@
+import com.amazonaws.services.sqs.model.Message;
+import com.bgu.dsp.awsUtils.SQSUtils;
+import com.bgu.dsp.awsUtils.Utils;
+import com.bgu.dsp.common.protocol.MalformedMessageException;
+import com.bgu.dsp.common.protocol.managertoworker.ManagerToWorkerCommand;
+import com.bgu.dsp.common.protocol.managertoworker.ManagerToWorkersSQSProtocol;
+import org.apache.log4j.Logger;
+
+public class Worker implements Runnable{
+    final static Logger log = Logger.getLogger(Worker.class);
+    private final String inQueueUrl;
+
+    public static void main(String args[]) {
+        SQSUtils.createQueue(Utils.MANAGER_TO_WORKERS_QUEUE_NAME);
+
+
+        Worker worker = new Worker();
+        worker.run();
+    }
+
+    public Worker() {
+        inQueueUrl = SQSUtils.getQueueUrlByName(Utils.MANAGER_TO_WORKERS_QUEUE_NAME);
+    }
+
+    public void run() {
+        while (true) {
+            String msg = getSqsMessageFromQueue();
+            ManagerToWorkerCommand cmd=null;
+            try {
+                cmd = ManagerToWorkersSQSProtocol.parse(msg);
+            }
+            catch (MalformedMessageException e) {
+                malformedMessage(e);
+                throw new RuntimeException("failed to parse message");
+            }
+            cmd.execute();
+        }
+    }
+
+
+    private String getSqsMessageFromQueue() {
+        Message msg = SQSUtils.getMessage(inQueueUrl);
+        return msg.getBody();
+    }
+
+    private void malformedMessage(MalformedMessageException e) {
+    }
+}
