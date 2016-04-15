5ffcadd38e2a96cc31049f883afe91746f42116d
hagai-lvi <hagai.lvi@gmail.com>
2016-04-15 20:01:15 +0300

    Manager deletes reply from worker

--- scripts/upload_jar.sh
+++ scripts/upload_jar.sh
@@ -1,12 +1,19 @@
-mvn install --file common/pom.xml
 
-mvn --file manager/pom.xml clean compile assembly:single -DskipTests
+# use maven production profile that does not include aws sdk in the jar
+mvn_profile=production
+
+mvn --activate-profiles ${mvn_profile} install --file common/pom.xml -DskipTests
+
+mvn --activate-profiles ${mvn_profile} --file manager/pom.xml clean
+mvn --activate-profiles ${mvn_profile} --file manager/pom.xml compile assembly:single -DskipTests
 cp manager/target/dsp-1-manager-1.0-SNAPSHOT-jar-with-dependencies.jar production
 
-mvn --file local/pom.xml clean compile assembly:single -DskipTests
+mvn --activate-profiles ${mvn_profile} --file local/pom.xml clean
+mvn --activate-profiles ${mvn_profile} --file local/pom.xml compile assembly:single -DskipTests
 cp local/target/dsp1-1.0-SNAPSHOT-jar-with-dependencies.jar production
 
-mvn --file worker/pom.xml clean compile assembly:single -DskipTests
-ccp worker/target/dsp-1-worker-1.0-SNAPSHOT-jar-with-dependencies.jar production
+mvn --activate-profiles ${mvn_profile} --file worker/pom.xml clean
+mvn --activate-profiles ${mvn_profile} --file worker/pom.xml compile assembly:single -DskipTests
+cp worker/target/dsp-1-worker-1.0-SNAPSHOT-jar-with-dependencies.jar production
 
 # aws s3 cp production s3://dsp-jars --recursive --exclude "*" --include "*.jar"
