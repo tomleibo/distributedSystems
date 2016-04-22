38e4e4b3b3ccd4ccaf8327c11e37e314353d8424
hagai-lvi <hagai.lvi@gmail.com>
2016-04-22 18:15:06 +0300

    Added message to build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -20,4 +20,12 @@ mvn --activate-profiles ${mvn_profile} --file worker/pom.xml clean
 mvn --activate-profiles ${mvn_profile} --file worker/pom.xml compile assembly:single -DskipTests
 cp worker/target/dsp-1-worker-1.0-SNAPSHOT-jar-with-dependencies.jar production
 
-# aws s3 cp production s3://dsp-jars --recursive --exclude "*" --include "*.jar"
+# upload all the jars in the production dir, and make it public
+
+RED='\033[0;31m'
+NO_COLOR='\033[0m'
+
+echo "${RED}* ------------------------------------------------------------------------------------------------ *"
+echo '* In order to upload the jars to s3 run the following command:                                     *'
+echo '* aws s3 cp production s3://dsp-jars --recursive --exclude "*" --include "*.jar" --acl public-read *'
+echo "* ------------------------------------------------------------------------------------------------ *${NO_COLOR}"
